c3d24d7363396de4c919f821c46da4f8740accee
Jacob Logan <91486310+jacoblogan98@users.noreply.github.com>
2023-02-19 16:58:53 -0500

    passed tests

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,20 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for i, dwarf in enumerate(dwarves):
+        print(f"{i+1}. {dwarf}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_calls):
+    return [f"{call.capitalize()}!" for call in planeteer_calls]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(planeteer_calls):
+    for call in planeteer_calls:
+        if len(call) > 4:
+            return True
+    
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(foods):
+    for food in foods:
+        if food == "cheddar" or food == "gouda" or food == "camembert":
+            return food
+    
+    return None
